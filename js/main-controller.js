6816276974b1bf99ca0b899acf815afaf1bdb2dc
Fernando Orozco <forozco2085@gmail.com>
2016-01-03 18:29:41 -0800

    initial commit

---
+++ js/main-controller.js
@@ -0,0 +1,40 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('app')
+    .controller("MainController", MainController);
+
+  MainController.$inject = [];
+
+  function MainController () {
+    var vm = this;
+    var pokemon = {
+      image_uri:  "",
+      about:      "pokemon battle simulation game",
+      name:       "Pokemon Battle",
+      tech_used:  ["Html 5", "Css 3", "Javascript"]
+    };
+    var hat = {
+      image_uri:  "",
+      about:      "Real time location of food trucks nearby",
+      name:       "Hungry as Truck",
+      tech_used:  ["Node.js", "Express", "Mongo"]
+    };
+    var openGym = {
+      image_uri:  "",
+      about:      "user generated info for basketball courts nearby",
+      name:       "Open Gym",
+      tech_used:  ["Html 5", "Css 3", "Javascript"]
+    };
+    var top5 = {
+      image_uri:  "",
+      about:      "lists of top 5 food in all category",
+      name:       "Top 5",
+      tech_used:  ["Ruby on Rails", "Postgresql", "Javascript"]
+    };
+    vm.message = "Hello World!";
+    vm.project = top5;
+  }
+
+})();
---
+++ js/main.js
@@ -0,0 +1,18 @@
+var str = `<h1 class='name'>Fernando Orozco</h1><p>-Web Developer-</p>`,
+    i = 0,
+    isTag,
+    text;
+
+(function type() {
+    text = str.slice(0, ++i);
+    if (text === str) return;
+
+    document.getElementById('typewriter').innerHTML = text;
+
+    var char = text.slice(-1);
+    if( char === '<' ) isTag = true;
+    if( char === '>' ) isTag = false;
+
+    if (isTag) return type();
+    setTimeout(type, 80);
+}());
