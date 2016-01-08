86a1ac7761eed9a05e482a5a8d917a7bd7a59d7e
Fernando Orozco <forozco2085@gmail.com>
2016-01-07 17:34:39 -0800

    dynamic background color for project page

--- js/main-controller.js
+++ js/main-controller.js
@@ -13,25 +13,33 @@
       image_uri:  "https://i.imgur.com/Y6Nie6i.png",
       about:      "pokemon battle simulation game: a simple game where you get to choose a pokemon and choose attack like the original gameboy game.",
       name:       "Pokemon Battle",
-      tech_used:  ["Html 5", "Css 3", "Javascript"]
+      tech_used:  ["Html 5", "Css 3", "Javascript"],
+      link:       "banando.github.io/pokemon",
+      color:      "#54D0F6"
     };
     var hat = {
       image_uri:  "https://i.imgur.com/69Q3mUH.png",
       about:      "Real time location of food trucks nearby",
       name:       "Hungry as Truck",
-      tech_used:  ["Node.js", "Express", "Mongo"]
+      tech_used:  ["Node.js", "Express", "Mongo"],
+      link:       "https://quiet-savannah-8403.herokuapp.com/",
+      color:      "#C0392B"
     };
     var openGym = {
       image_uri:  "https://i.imgur.com/RCYfPlu.png",
       about:      "user generated info for basketball courts nearby",
       name:       "Open Gym",
-      tech_used:  ["Html 5", "Css 3", "Javascript"]
+      tech_used:  ["Html 5", "Css 3", "Javascript"],
+      link:       "https://protected-dusk-8947.herokuapp.com",
+      color:      "#C2904C"
     };
     var top5 = {
       image_uri:  "https://i.imgur.com/tbccbhQ.png",
       about:      "lists of top 5 food in all category",
       name:       "Top 5",
-      tech_used:  ["Ruby on Rails", "Postgresql", "Javascript"]
+      tech_used:  ["Ruby on Rails", "Postgresql", "Javascript"],
+      link:       "https://calm-scrubland-2209.herokuapp.com",
+      color:      "#595959"
     };
     vm.projects = [pokemon, top5, hat, openGym];
     vm.project = pokemon;
